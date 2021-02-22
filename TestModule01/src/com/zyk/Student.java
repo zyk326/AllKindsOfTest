842260f3f3f4c0a3c7a8fc06fcb441aa069e3f75
zyk326 <869550165@qq.com>
2021-02-22 17:42:12 +0800

    添加了HaspMap

--- TestModule01/src/com/zyk/Student.java
+++ TestModule01/src/com/zyk/Student.java
@@ -1,6 +1,16 @@
 package com.zyk;
 
+import java.util.HashMap;
+
 public class Student {
-    private int age;
-    private String name;
+    public static void main(String[] args) {
+        int age;
+        String name;
+        HashMap <String, String> Arr = new HashMap<String, String>();
+        Arr.put("nian", "12");
+        Arr.put("zyk","15");
+        System.out.println(Arr.get("nian"));
+        System.out.println(Arr.get("zyk"));
+    }
+
 }
